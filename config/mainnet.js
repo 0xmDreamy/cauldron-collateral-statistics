6252eea6b06de43178a7a99df8097b0f802b37e4
0xmDreamy <0xmDreamy@proton.me>
2024-09-05 00:01:25 +0200

    add sdeusd v2 cauldron

--- config/mainnet.js
+++ config/mainnet.js
@@ -6,5 +6,10 @@ module.exports = {
 			address: "0x00380CB5858664078F2289180CC32F74440AC923",
 			startBlock: 20423950,
 		},
+		{
+			name: "sdeUSD V2",
+			address: "0x38E7D1e4E2dE5b06b6fc9A91C2c37828854A41bb",
+			startBlock: 20613403,
+		},
 	],
 };
